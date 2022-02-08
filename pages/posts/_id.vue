91b28016f1f45d07c344c69ac599e22e2646be17
ongomobile <ongomobile@gmail.com>
2022-02-08 15:50:07 -0800

    First commit

---
+++ pages/posts/_id.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">{{ post.title }}</h1>
+      <p>{{ post.content }}</p>
+    </article>
+    <aside>
+      <h3>Post you might enjoy</h3>
+      <ul>
+        <li v-for="related in relatedPosts">
+          <nuxt-link :to="{ name: 'posts-id', params: { id: related.id } }">{{
+            related.title
+          }}</nuxt-link>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: this.post.title,
+      meta: [
+        { name: "twitter:title", content: "Nuxt Fundamentals by Vue School" },
+        { name: "twitter:description", content: "Nuxt + Vue School = 🍕" },
+        { name: "twitter:image", content: "https://i.imgur.com/UYP2umJ.png" },
+        { name: "twitter:card", content: "summary_large_image" },
+      ],
+    };
+  },
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+  computed: {
+    post() {
+      return this.$store.state.posts.all.find((p) => p.id === this.id);
+    },
+    relatedPosts() {
+      return this.$store.state.posts.all.filter((p) => p.id !== this.id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  line-height: 1.5;
+}
+article * {
+  margin-bottom: 1rem;
+}
+aside {
+  min-width: 280px;
+  max-width: 280px;
+  padding-left: 2rem;
+}
+.title {
+  font-size: 2rem;
+}
+</style>
